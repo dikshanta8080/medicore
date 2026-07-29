7994d1a4435efe284945e601adf59227a5761750
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-07-29 12:39:45 +0545

    (feat): Implement Appointment model

---
+++ src/main/java/com/acharya/dikshanta/HospitalManagement/appointment/repository/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package com.acharya.dikshanta.HospitalManagement.appointment.repository;
+
+import com.acharya.dikshanta.HospitalManagement.appointment.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+}
