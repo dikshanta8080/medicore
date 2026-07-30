7e2cb465b391f3f7ed896eeb3f9fac9bf3b8ba1d
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-07-30 20:55:31 +0545

    (reafractor): Add null checks in the Doctor mapper

--- src/main/java/com/acharya/dikshanta/HospitalManagement/doctor/mapper/DoctorMapper.java
+++ src/main/java/com/acharya/dikshanta/HospitalManagement/doctor/mapper/DoctorMapper.java
@@ -3,10 +3,13 @@ package com.acharya.dikshanta.HospitalManagement.doctor.mapper;
 import com.acharya.dikshanta.HospitalManagement.doctor.dto.request.CreateDoctorRequest;
 import com.acharya.dikshanta.HospitalManagement.doctor.dto.response.DoctorResponse;
 import com.acharya.dikshanta.HospitalManagement.doctor.model.Doctor;
+import com.acharya.dikshanta.HospitalManagement.identity.model.Staff;
+import com.acharya.dikshanta.HospitalManagement.identity.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DoctorMapper {
+
     public Doctor toEntity(CreateDoctorRequest request) {
         return Doctor.builder()
                 .consultationFee(request.consultationFee())
@@ -15,17 +18,29 @@ public class DoctorMapper {
     }
 
     public DoctorResponse toResponse(Doctor doctor) {
+
+        Staff staff = doctor.getStaff();
+        User user = staff != null ? staff.getUser() : null;
+
         return DoctorResponse.builder()
-                .name(doctor.getStaff().getName())
-                .email(doctor.getStaff().getUser().getEmail())
-                .username(doctor.getStaff().getUser().getUsername())
-                .address(doctor.getStaff().getAddress())
-                .phoneNumber(doctor.getStaff().getPhoneNumber())
-                .gender(doctor.getStaff().getGender())
+                .name(staff != null ? staff.getName() : null)
+                .email(user != null ? user.getEmail() : null)
+                .username(user != null ? user.getUsername() : null)
+                .address(staff != null ? staff.getAddress() : null)
+                .phoneNumber(staff != null ? staff.getPhoneNumber() : null)
+                .gender(staff != null ? staff.getGender() : null)
                 .consultationFee(doctor.getConsultationFee())
                 .licenseNumber(doctor.getLicenseNumber())
-                .specialization(doctor.getSpecialization().getName())
-                .department(doctor.getDepartment().getName())
+                .specialization(
+                        doctor.getSpecialization() != null
+                                ? doctor.getSpecialization().getName()
+                                : null
+                )
+                .department(
+                        doctor.getDepartment() != null
+                                ? doctor.getDepartment().getName()
+                                : null
+                )
                 .build();
     }
 }
