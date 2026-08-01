629361ab10d3d31a6af2ac9018782f9f7c2ddda6
prakash69-code <neupaneprakash9999@gmail.com>
2026-08-01 14:23:49 +0545

    feat(billing): create mappers for payment and billing workflow

--- src/main/java/com/acharya/dikshanta/HospitalManagement/billing/dto/response/InvoiceItemResponse.java
+++ src/main/java/com/acharya/dikshanta/HospitalManagement/billing/dto/response/InvoiceItemResponse.java
@@ -1,10 +1,12 @@
 package com.acharya.dikshanta.HospitalManagement.billing.dto.response;
 
 import com.acharya.dikshanta.HospitalManagement.billing.model.ItemType;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Builder
 public record InvoiceItemResponse(
         UUID id,
         String description,
